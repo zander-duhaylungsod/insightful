10d0c83e237efc22a8fa901d400f35cc5171324f
Zander Duhaylungsod <duhaylungsodzander@gmail.com>
2024-10-06 21:46:50 +0800

    Update intro.js

--- javascript/intro.js
+++ javascript/intro.js
@@ -1,18 +1,15 @@
-// document.addEventListener('DOMContentLoaded', function() {
-    // var msg = new SpeechSynthesisUtterance();
-    // msg.text = "Welcome to INSIGHTFUL! Press anywhere to start the app.";
-    // // Add a delay of 2 seconds (2000 milliseconds)
+document.addEventListener('DOMContentLoaded', function() {
     setTimeout(function() {
         readObjectAloud("Welcome to INSIGHTFUL! Press anywhere to start the app.");
+        // Moved inside a touch event to ensure it works on mobile
+        document.addEventListener('click', function() {            
+            var overlay = document.getElementById('overlay');
+            overlay.classList.add('expand');
+            setTimeout(function() {
+                window.location.href = 'objectdetector.html';
+            }, 500);  // Match this duration with the CSS transition duration
+        }, { once: true });  // Ensure this only runs once
     }, 500);
-// });
-
-document.addEventListener('click', function() {
-    var overlay = document.getElementById('overlay');
-    overlay.classList.add('expand');
-    setTimeout(function() {
-        window.location.href = 'objectdetector.html';
-    }, 500); // Match this duration with the CSS transition duration
 });
 
 // Function to read out text via speech synthesis
@@ -20,3 +17,4 @@ function readObjectAloud(text) {
     const speech = new SpeechSynthesisUtterance(text);
     window.speechSynthesis.speak(speech);
 }
+
